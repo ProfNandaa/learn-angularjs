3ce652ad82f02948da16a13848958e2d319f1f2c
@ProfNandaa <profnandaa@gmail.com>
2015-05-27 20:36:30 +0300

    AngularJS, NodeJS Contacts app

---
+++ contacts/server.js
@@ -0,0 +1,14 @@
+var express = require('express')
+	, api = require('./api')
+	, app = express();
+
+
+app
+	.use(express.static('./public'))
+	.use('/api',api)
+	.get('*', function(req,res){
+		res.sendFile(__dirname + '/public/main.html');
+	})
+	.listen(3040,function(){
+		console.log("Express server listening on port 3040");
+	});
