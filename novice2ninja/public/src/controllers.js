5608a4f8bd6df3a72b9c86bf66c6ce44ba370f94
@ProfNandaa <profnandaa@gmail.com>
2015-06-10 18:45:13 +0300

    Weather App - controller

--- novice2ninja/public/src/controllers.js
+++ novice2ninja/public/src/controllers.js
@@ -131,4 +131,15 @@ angular.module('myApp')
 					//of error
 			});
 		}
+	})
+	.controller('WeatherController',function($scope,weatherService){
+		$scope.getWeather = function(){
+			$scope.weatherDescription = "Fetching...";
+			weatherService.getWeather($scope.city,$scope.country)
+				.then(function(data){
+					$scope.weatherDescription = data;
+				}, function(){
+					$scope.weatherDescription = "Could not obtain data";
+				});
+		}
 	});
